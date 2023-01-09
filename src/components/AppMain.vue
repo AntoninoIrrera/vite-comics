a651bdceba7c8b15ac2a372abda62ef1bbc32ba1
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-09 15:18:25 +0100

    inizio creazione contenuti main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,4 @@
-<script >
+<script>
 
 
 
@@ -7,9 +7,35 @@
 </script>
 
 <template>
+    <main>
 
+        <section class="riga-nera">
+            <h2 class="contenuto-main">Content goes here</h2>
+        </section>
+        <section class="riga-blu"></section>
+
+    </main>
 </template>
 
 <style scoped>
+
+    .riga-nera{
+        height: 150px;
+        background-color: black;
+        display: flex;
+        align-items: center;
+    }
+
+    .riga-blu{
+        height: 150px;
+        background-color: blue;
+    }
+
+    .contenuto-main{
+        color: white;
+        margin-left: 20rem;
+    }
+
+
 
 </style>
