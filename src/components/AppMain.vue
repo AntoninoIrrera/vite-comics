c3406dccb803157a723bdb436afc252334d0837f
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-09 16:27:00 +0100

    aggiunta bonus inserimento dinamico contenuti riga blu

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,39 @@
 <script>
 
 
+export default {
+    data() {
+        return {
+            images: [
+                {
+                    image: '../assets/img/buy-comics-digital-comics.png',
+                    title: 'digital comics',
+                }, {
+                    image: '../assets/img/buy-comics-merchandise.png',
+                    title: 'DC Merchandise',
+                }, {
+                    image: '../assets/img/buy-comics-subscriptions.png',
+                    title: 'Subscription',
+                }, {
+                    image: '../assets/img/buy-comics-shop-locator.png',
+                    title: 'Comic Shop Locator',
+                }, {
+                    image: '../assets/img/buy-dc-power-visa.svg',
+                    title: "Dc power visa",
+                }
+            ]
+        }
+    },
+    methods: {
+        getImagePath(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+     
+    }
+
+}
+
+   
 
 
 
@@ -12,30 +45,46 @@
         <section class="riga-nera">
             <h2 class="contenuto-main">Content goes here</h2>
         </section>
-        <section class="riga-blu"></section>
+        <section class="riga-blu">
+            <div v-for="image in images">
+                <img :src="getImagePath(image.image)" :alt="image.title">
+                <h4>{{image.title}}</h4>
+            </div>
+        </section>
 
     </main>
 </template>
 
 <style scoped>
-
-    .riga-nera{
+.riga-nera{
         height: 150px;
         background-color: black;
         display: flex;
+        justify-content: center;
         align-items: center;
     }
 
     .riga-blu{
-        height: 150px;
+        /* height: 150px; */
+        padding: 3rem;
         background-color: blue;
+        display: flex;
+        justify-content: center;
     }
 
+    img{
+        width: 15%;
+    }
+
+    h4{
+        display: inline-block;
+        color: white;
+        vertical-align: super;
+        margin-left: 0.75rem;
+    }
     .contenuto-main{
         color: white;
         margin-left: 20rem;
     }
-
-
 
 </style>
