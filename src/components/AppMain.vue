396eff520b3e6bceca7f31ae0735a4d9e9fcf4f5
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-10 15:27:04 +0100

    inserimento dinamicamente current series nel main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,87 @@
 <script>
+import CurrentSeries from './CurrentSeries.vue';
 
 
 export default {
+    components:{
+        CurrentSeries,
+    },
     data() {
         return {
+            currentSeries: [
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ],
             images: [
                 {
                     image: '../assets/img/buy-comics-digital-comics.png',
@@ -43,11 +121,14 @@ export default {
     <main>
 
         <section class="riga-nera">
-            <h2 class="contenuto-main">Content goes here</h2>
+            <div v-for="serie in currentSeries" class="contenitoreImg">
+                <CurrentSeries :pathImg="serie.thumb" :title="serie.series"/>
+            </div>
         </section>
+
         <section class="riga-blu">
             <div v-for="image in images">
-                <img :src="getImagePath(image.image)" :alt="image.title">
+                <img class="imgRigaBlu" :src="getImagePath(image.image)" :alt="image.title">
                 <h4>{{image.title}}</h4>
             </div>
         </section>
@@ -57,10 +138,14 @@ export default {
 
 <style scoped>
 .riga-nera{
-        height: 150px;
         background-color: black;
         display: flex;
-        align-items: center;
+        flex-wrap: wrap;
+        padding: 4rem;
+    }
+
+    .contenitoreImg {
+        width: calc(100% / 6);
     }
 
     .riga-blu{
@@ -71,7 +156,7 @@ export default {
         /* align-items: center; */
     }
 
-    img{
+    .imgRigaBlu{
         width: 15%;
     }
 
@@ -81,9 +166,4 @@ export default {
         vertical-align: super;
         margin-left: 0.75rem;
     }
-    .contenuto-main{
-        color: white;
-        margin-left: 20rem;
-    }
-
 </style>
---
+++ src/components/CurrentSeries.vue
@@ -0,0 +1,33 @@
+<script>
+
+
+export default{
+    props:{
+        pathImg: String,
+        title: String,
+    }
+}
+
+
+
+
+</script>
+
+<template>
+   
+    <img :src="pathImg" :alt="title">
+    <h4>{{ title }}</h4>
+
+</template>
+
+<style scoped>
+   
+    img{
+        width: 60%;
+    }
+
+    h4{
+        color: white;
+        width: 60%;
+    }
+</style>
